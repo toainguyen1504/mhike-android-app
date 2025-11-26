0510d2520d9e062da8f3dd3250484b0cd7cb4671
toainguyen1504 <toaincgcss200874@fpt.edu.vn>
2025-11-26 17:43:07 +0700

    feat: add splash screen and basic color setup

--- app/src/main/java/com/example/mhikeandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/mhikeandroidapp/MainActivity.kt
@@ -4,28 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.mhikeandroidapp.ui.theme.MhikeAndroidAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        installSplashScreen()
+
         setContent {
             MhikeAndroidAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+
+                ) {
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
+
         }
     }
 }
